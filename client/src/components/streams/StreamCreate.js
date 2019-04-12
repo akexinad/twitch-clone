a260cb08d7f967630051d14b9562729fa492a2b7
danny <danixeka@gmail.com>
2019-04-12 17:10:29 +1000

    Displaying errors on touch and highlighting error fields

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -3,7 +3,19 @@ import { Field, reduxForm } from 'redux-form'
 //  Field is a react component, and reduxForm is a function
 
 class StreamCreate extends React.Component {
-  renderInput({ input, label, meta }) {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">
+            { error }
+          </div>
+        </div>
+      )
+    }
+  }
+
+  renderInput = ({ input, label, meta }) => {
     // return (
     //   <input
     //     onChange={ formProps.input.onChange }
@@ -11,11 +23,12 @@ class StreamCreate extends React.Component {
     //   />
     // )
     // SHORTENED SYNTAX
+    const className = `field ${ meta.error && meta.touched ? 'error' : '' }`
     return (
-      <div className="field">
+      <div className={ className }>
         <label>{ label }</label>
-        <input { ...input }/>
-        <div>{ meta.error }</div>
+        <input { ...input } autoComplete="off" />
+        <div>{ this.renderError(meta) }</div>
       </div>
     )
   }
@@ -28,7 +41,7 @@ class StreamCreate extends React.Component {
     return (
       <form
         onSubmit={ this.props.handleSubmit(this.onSubmit) }
-        className="ui form"
+        className="ui form error"
       >
         <Field
           name="title"
